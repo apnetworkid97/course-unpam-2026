768013303dcc22783cb85eef0933222735a3743f
aditya agus <adityaagus161@gmail.com>
2026-01-16 21:39:55 +0700

    update day-5 FINAL

--- docs/day-5/backends/src/blockchain/helpers/deployments.ts
+++ docs/day-5/backends/src/blockchain/helpers/deployments.ts
@@ -1,5 +1,5 @@
-import dotenv from 'dotenv';
-dotenv.config();
+// import dotenv from 'dotenv';
+// dotenv.config();
 import { avalancheFuji } from 'viem/chains';
 
 export const USER_CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS as `0x${string}`;
